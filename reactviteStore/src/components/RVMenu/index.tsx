8567bdd1a925f6e57d37b645a2447f9bf98c89fd
Edouard <edurand.viel@gmail.com>
2026-03-26 14:10:07 +0100

    contect useer with useMemo

--- reactviteStore/src/components/RVMenu/index.tsx
+++ reactviteStore/src/components/RVMenu/index.tsx
@@ -2,17 +2,21 @@ import { NavLink } from "react-router";
 import { Menu } from "../RVLayout/styles";
 
 import { connect } from "react-redux";
-import {useContext } from "react";
-import { UserContext } from "../../contexts/UserContext";
+import {useContext, useMemo } from "react";
+import { userContext } from "../../contexts/UserContext";
 
 const Index = () => {
-  const CurrentUser = useContext(UserContext);
+  const currentUser = useContext(userContext);
+  const contextValue = useMemo(() => ({
+    currentUser,
+  }), [currentUser]);
+
   return (
     <Menu>
       <NavLink className="links" to="/">
         Home
       </NavLink>
-      {CurrentUser && (<>
+      {contextValue && (<>
         <NavLink className="links" to="/user/add">
           Create user
         </NavLink>
