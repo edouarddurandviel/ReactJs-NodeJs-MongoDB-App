df4665eaa009d4e3ef404d645b159ebf37c025e5
Edouard <edurand.viel@gmail.com>
2026-04-11 10:26:09 +0200

    optimized

--- reactviteStore/src/api/buildUrl.ts
+++ reactviteStore/src/api/buildUrl.ts
@@ -1,64 +1,76 @@
 import type { PathParamsObject, QueryObject } from "./interfaces";
 
-const getParam = (path: string) => {
-  const el = path.split("/");
-  return el.filter((e) => e.startsWith(":")).map((e) => e.substring(1));
-};
+// optimized
+const getParam = (path: string): string[] => {
+  const result: string[] = [];
+  let start = -1;
 
-const verifyParams = (params: Array<string>, receivedParams: PathParamsObject) => {
-  const receivedMap = receivedParams;
-  const paramsM: PathParamsObject = {};
-  params.forEach((e) => Object.assign(paramsM, { [e]: "" }));
-  Object.keys(paramsM).forEach((e) => {
-    if (Object.keys(receivedMap).includes(e)) {
-      paramsM[e] = receivedMap[e]!;
+  for (let i = 0; i < path.length; i++) {
+    const char = path[i];
+
+    if (char === ":") {
+      start = i + 1;
+    } else if (start !== -1 && char === "/") {
+      result.push(path.slice(start, i));
+      start = -1;
     }
-  });
-  return paramsM;
+  }
+
+  if (start !== -1) {
+    result.push(path.slice(start));
+  }
+
+  return result;
 };
 
-const createUrl = (path: string, buildParams: PathParamsObject) => {
-  let newPath = "";
-  for (const [key, value] of Object.entries(buildParams)) {
-    if (key) {
-      const regexp = new RegExp(`:${key}`, "i");
-      newPath = newPath.length ? newPath.replace(regexp, `${value}`) : path.replace(regexp, `${value}`);
+// optimized
+const verifyParams = (
+  params: string[],
+  receivedParams: PathParamsObject
+): PathParamsObject => {
+  const result: PathParamsObject = {};
+
+  for (let i = 0; i < params.length; i++) {
+    const key = params[i];
+    if(key)
+      result[key] = key in receivedParams ? receivedParams[key]! : "";
+  }
+
+  return result;
+};
+
+// optimized
+const createUrl = (path: string, buildParams: PathParamsObject): string => {
+  let newPath = path;
+  for (const key in buildParams) {
+    const value = buildParams[key];
+    if (value !== undefined) {
+      newPath = newPath.replace(`:${key}`, String(value));
     }
   }
   return newPath;
 };
 
-const serialize = (query: QueryObject) => {
-  let serialized: string = "?";
-  for (let key in query) {
-    if (query.hasOwnProperty(key)) {
-      if (serialized !== "?") {
-        serialized += "&";
-      }
-      serialized += key + "=" + query[key];
-    }
-  }
-  return serialized;
+// optimized
+const serialize = (query: QueryObject): string => {
+  const queryString = new URLSearchParams(query as Record<string, string>).toString();
+  return queryString ? `?${queryString}` : "";
 };
 
-export const urlBuilder = (path: string, params?: PathParamsObject, query?: QueryObject) => {
+export const urlBuilder = (path: string, params?: PathParamsObject, query?: QueryObject): string => {
   const Params = getParam(path);
   let serialized: string | null = null;
+  if (query) {
+    serialized = serialize(query);
+  }
 
   if (params) {
     const verifiedParamObject = verifyParams(Params, params);
 
-    if (query) {
-      serialized = serialize(query);
-    }
+    return !serialized ? createUrl(path, verifiedParamObject) : `${path}${serialized}`;
 
-    const url = verifiedParamObject ? createUrl(path, verifiedParamObject) : `${(serialized && path + serialized) || path}`;
-
-    return url;
   } else {
-    if (query) {
-      serialized = serialize(query);
-    }
-    return `${(serialized && path + serialized) || path}`;
+   
+    return !serialized ? path : `${path}${serialized}`;
   }
 };
