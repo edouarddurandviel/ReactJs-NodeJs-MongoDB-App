592ffc65bfd2e5b6b513f1ee6e3798cca5ba74a4
Edouard <edurand.viel@gmail.com>
2026-04-24 19:37:58 +0200

    comments for current running operations

--- reactviteStore/src/api/apiCache.ts
+++ reactviteStore/src/api/apiCache.ts
@@ -11,21 +11,26 @@ export default class apiCache {
     return this.cache.get(key);
   };
 
+  // operation: (undefined/trrue) should or should not refresh data
   public cacheRequest = (key: any, config: any) => {
     const item = key.split("/")[0];
     const whiteListed = this.cacheConfig.whitelist.filter((e: string) => e === item);
 
+    // if route has been registeted in the previous whiteListe
+    // do not proceed to any request
     if (whiteListed === undefined) {
       return false;
     } else {
+      // check if there is any current proceeding operations
       if (this.cache.has(key) && !this.cache.has("operation")) {
-        // cancel
+        // cancel cache request
         return true;
       } else if (
         config.method === "patch" || 
         config.method === "post" || 
         config.method === "delete"
       ) {
+        // for any writing requests  
         this.cache.set("operation", true);
         return false;
       }
@@ -33,6 +38,7 @@ export default class apiCache {
   };
 
   public manageResponse = (response: any) => {
+    // For any read requests consume operation
     if (response && response.config.method === "get") {
       if (this.cache.has("operation")) {
         this.cache.delete("operation");
@@ -41,6 +47,7 @@ export default class apiCache {
 
       return response;
     } else {
+      // for any writing running operations add operation to cache
       this.cache.set("operation", true);
 
       return response;
